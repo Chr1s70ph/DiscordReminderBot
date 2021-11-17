e8118dc5124bb853d2b5316dc73556e870a165b4
Christoph <29735603+Chr1s70ph@users.noreply.github.com>
2021-11-17 23:18:56 +0100

    add new games

--- commands/start.js
+++ commands/start.js
@@ -11,18 +11,21 @@ const pm2 = require("pm2")
 const fetch = require("node-fetch")
 
 const defaultApplications = {
-	youtube: "880218394199220334", // Note : First package to include the new YouTube Together version, any other package offering it will be clearly inspired by it
-	youtubedev: "880218832743055411", // Note : First package to include the new YouTube Together development version, any other package offering it will be clearly inspired by it
+	youtube: "880218394199220334",
+	youtubedev: "880218832743055411",
 	poker: "755827207812677713",
 	betrayal: "773336526917861400",
 	fishing: "814288819477020702",
 	chess: "832012774040141894",
-	chessdev: "832012586023256104", // Note : First package to offer chessDev, any other package offering it will be clearly inspired by it
-	lettertile: "879863686565621790", // Note : First package to offer lettertile, any other package offering it will be clearly inspired by it
-	wordsnack: "879863976006127627", // Note : First package to offer wordsnack any other package offering it will be clearly inspired by it
-	doodlecrew: "878067389634314250", // Note : First package to offer doodlecrew, any other package offering it will be clearly inspired by it
-	awkword: "879863881349087252", // Note : First package to offer awkword, any other package offering it will be clearly inspired by it
-	spellcast: "852509694341283871" // Note : First package to offer spellcast, any other package offering it will be clearly inspired by it
+	chessdev: "832012586023256104",
+	lettertile: "879863686565621790",
+	wordsnack: "879863976006127627",
+	doodlecrew: "878067389634314250",
+	awkword: "879863881349087252",
+	spellcast: "852509694341283871",
+	checkers: "832013003968348200",
+	puttparty: "763133495793942528",
+	sketchyartist: "879864070101172255"
 }
 
 exports.name = "start"
